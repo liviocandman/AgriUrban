bb2ff27ac7a980efc2db5dbc5a441d0777902a82
liviocandman <liviocandman@gmail.com>
2023-05-19 17:54:23 -0300

    base project

---
+++ src/theme/index.ts
@@ -0,0 +1,42 @@
+export const THEME = {
+  COLORS: {
+    BACKGROUND_900: '#FAFAF9',
+    BACKGROUND_800: '#F5F5F4',
+    BACKGROUND_700: '#F1F1F1',
+
+    TEXT_100: '#292524',
+    TEXT_200: '#78716C',
+
+    CAPTION_500: '#71717A',
+    CAPTION_400: '#A1A1AA',
+    CAPTION_300: '#D4D4D8',
+
+    ACCENT_100:'#F5BE0B',
+    ACCENT_200:'#FEF1C7',
+
+    SHAPE: '#2A2634',
+
+    PRIMARY_100: '#8B5CF6',
+    PRIMARY_200: '#F0FDF5',
+    PRIMARY_300: '#19725D',
+
+    SUCCESS: '#34D399',
+    ALERT: '#F87171',
+
+    FOOTER: ['rgba(0,0,0,0)', 'rgba(0,0,0,0.9)'],
+    OVERLAY: 'rgba(0,0,0,0.6)',
+  },
+
+  FONT_FAMILY: {
+    REGULAR: 'Inter_400Regular',
+    SEMI_BOLD: 'Inter_600SemiBold',
+    BOLD: 'Inter_700Bold',
+    BLACK: 'Inter_900Black'
+  },
+
+  FONT_SIZE: {
+    SM: 14,
+    MD: 16,
+    LG: 24
+  }
+};
